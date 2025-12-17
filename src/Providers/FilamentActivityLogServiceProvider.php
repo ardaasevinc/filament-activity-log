166e41045c181553fa1fff3c73ef848c0ccff175
ardaasevinc <ardaasevinc@gmail.com>
2025-12-17 10:29:29 +0300

    test

--- src/Providers/FilamentActivityLogServiceProvider.php
+++ src/Providers/FilamentActivityLogServiceProvider.php
@@ -14,9 +14,14 @@ class FilamentActivityLogServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if ($this->app->runningInConsole()) {
         $this->publishes([
-            __DIR__ . '/../../database/migrations/create_activity_logs_table.php.stub' =>
-                database_path('migrations/' . date('Y_m_d_His') . '_create_activity_logs_table.php'),
+                dirname(__DIR__, 2) . '/database/migrations/create_activity_logs_table.php.stub'
+                => database_path(
+                        'migrations/' . date('Y_m_d_His') . '_create_activity_logs_table.php'
+                    ),
         ], 'filament-activity-log-migrations');
     }
+}
+
 }
