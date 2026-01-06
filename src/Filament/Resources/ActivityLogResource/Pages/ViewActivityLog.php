c792ab067b56d108899c5dccee6c36c4df148c56
ardaasevinc <ardaasevinc@gmail.com>
2026-01-06 15:33:58 +0300

    test

--- src/Filament/Resources/ActivityLogResource/Pages/ViewActivityLog.php
+++ src/Filament/Resources/ActivityLogResource/Pages/ViewActivityLog.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\ActivityLogResource\Pages;
 use App\Filament\Resources\ActivityLogResource;
 use Filament\Resources\Pages\ViewRecord;
 
-class ViewActivityLogs extends ViewRecord
+class ViewActivityLog extends ViewRecord
 {
     protected static string $resource = ActivityLogResource::class;
 }
